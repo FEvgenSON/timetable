129dcfd3b851159bfc3270dd7e8808c48058f410
fevgenson <fevgenson@gmail.com>
2020-01-19 16:34:24 +0700

    Add MainActivity

---
+++ app/src/main/java/com/fevgenson/timetable/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.fevgenson.timetable.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fevgenson.timetable.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
