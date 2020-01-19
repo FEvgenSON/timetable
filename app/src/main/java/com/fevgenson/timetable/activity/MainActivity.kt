8bc9b496f31e1a86385eebf4bcfdec390acba0f0
fevgenson <fevgenson@gmail.com>
2020-01-19 17:04:38 +0700

    Main activity layout

--- app/src/main/java/com/fevgenson/timetable/activity/MainActivity.kt
+++ app/src/main/java/com/fevgenson/timetable/activity/MainActivity.kt
@@ -3,11 +3,15 @@ package com.fevgenson.timetable.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.fevgenson.timetable.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //action bar
+        setSupportActionBar(mainActivityToolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 }
