8c782b2d628f79199979440b59f5685299262375
fevgenson <fevgenson@gmail.com>
2020-01-21 14:22:37 +0700

    Add DiffUtils in recycler view adapter

--- app/src/main/java/com/fevgenson/timetable/adapter/LessonRecyclerViewAdapter.kt
+++ app/src/main/java/com/fevgenson/timetable/adapter/LessonRecyclerViewAdapter.kt
@@ -2,17 +2,33 @@ package com.fevgenson.timetable.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.fevgenson.timetable.R
 import com.fevgenson.timetable.room.entity.Lesson
 import com.fevgenson.timetable.viewholder.LessonViewHolder
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class LessonRecyclerViewAdapter : RecyclerView.Adapter<LessonViewHolder>() {
     private var data = listOf<Lesson>()
+    private var disposable: Disposable? = null
 
     fun update(data: List<Lesson>) {
-        this.data = data
-        notifyDataSetChanged()//TODO: add DiffUtils
+        disposable = Single.fromCallable {
+            return@fromCallable DiffUtil.calculateDiff(
+                LessonDiff(this.data, data)
+            )
+        }.subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                this.data = data
+                it.dispatchUpdatesTo(this)
+                disposable?.dispose()
+                disposable = null
+            }, {})
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LessonViewHolder {
@@ -31,5 +47,34 @@ class LessonRecyclerViewAdapter : RecyclerView.Adapter<LessonViewHolder>() {
 
     override fun onBindViewHolder(holder: LessonViewHolder, position: Int) {
         holder.onBind(data[position])
+    }
+
+    class LessonDiff(private val oldItems: List<Lesson>, private val newItems: List<Lesson>) :
+        DiffUtil.Callback() {
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldItems[oldItemPosition].name == newItems[newItemPosition].name
+        }
+
+        override fun getOldListSize(): Int {
+            return oldItems.size
+        }
+
+        override fun getNewListSize(): Int {
+            return newItems.size
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            val oldItem = oldItems[oldItemPosition]
+            val newItem = newItems[newItemPosition]
+            val same =
+                oldItem.teacher == newItem.teacher &&
+                        oldItem.building == newItem.building &&
+                        oldItem.classroom == newItem.classroom &&
+                        oldItem.type == newItem.type &&
+                        oldItem.time == newItem.time
+            //TODO: extension
+            return same
+        }
+
     }
 }
