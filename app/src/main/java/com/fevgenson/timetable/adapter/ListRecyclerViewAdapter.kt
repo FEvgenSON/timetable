3d2f379bb603634afe63a74964bc49b1367b95b7
fevgenson <fevgenson@gmail.com>
2020-01-24 12:57:34 +0700

    Add expandable settings

--- app/src/main/java/com/fevgenson/timetable/adapter/ListRecyclerViewAdapter.kt
+++ app/src/main/java/com/fevgenson/timetable/adapter/ListRecyclerViewAdapter.kt
@@ -3,12 +3,16 @@ package com.fevgenson.timetable.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.ChangeBounds
+import androidx.transition.TransitionManager
 import com.fevgenson.timetable.R
 import com.fevgenson.timetable.room.entity.ListWithLessons
 import com.fevgenson.timetable.viewholder.ListViewHolder
+import kotlinx.android.synthetic.main.view_list_item.view.*
 
 class ListRecyclerViewAdapter : RecyclerView.Adapter<ListViewHolder>() {
     private var data: List<ListWithLessons> = mutableListOf()
+    var expandedItemsId = mutableListOf<String>()
 
     fun update(newData: List<ListWithLessons>) {
         data = newData
@@ -16,13 +20,25 @@ class ListRecyclerViewAdapter : RecyclerView.Adapter<ListViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        return ListViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.view_list_item,
-                parent,
-                false
-            )
+        val holder = ListViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.view_list_item, parent, false)
         )
+        //expand when item click
+        holder.itemView.listItem.setOnClickListener {
+            val transition = ChangeBounds()
+            transition.duration = 200L
+            TransitionManager.beginDelayedTransition(parent, transition)
+            val position = holder.adapterPosition
+            val id = data[position].list
+            if (expandedItemsId.contains(id)) {
+                expandedItemsId.remove(id)
+                holder.setExpandedPartVisibility(false)
+            } else {
+                expandedItemsId.add(id)
+                holder.setExpandedPartVisibility(true)
+            }
+        }
+        return holder
     }
 
     override fun getItemCount(): Int = data.size
--- app/src/main/java/com/fevgenson/timetable/viewholder/ListViewHolder.kt
+++ app/src/main/java/com/fevgenson/timetable/viewholder/ListViewHolder.kt
@@ -1,13 +1,54 @@
 package com.fevgenson.timetable.viewholder
 
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.fevgenson.timetable.room.entity.ListWithLessons
 import kotlinx.android.synthetic.main.view_list_item.view.*
 
 class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private var expandedPartVisible = false
+
+    init {
+        val color = ContextCompat.getColor(
+            itemView.context,
+            android.R.color.darker_gray
+        )
+        itemView.arrow.colorFilter =
+            PorterDuffColorFilter(
+                color, PorterDuff.Mode.SRC_IN
+            )
+        itemView.edit_ex.colorFilter = PorterDuffColorFilter(
+            color, PorterDuff.Mode.SRC_IN
+        )
+        itemView.copy_ex.colorFilter = PorterDuffColorFilter(
+            color, PorterDuff.Mode.SRC_IN
+        )
+    }
 
     fun onBind(listWithLessons: ListWithLessons) {
         itemView.name.text = listWithLessons.list
+    }
+
+    fun setExpandedPartVisibility(visible: Boolean) {
+        if (visible == expandedPartVisible) {
+            return
+        }
+        expandedPartVisible = visible
+        if (visible) {
+            itemView.divider.visibility = View.VISIBLE
+            itemView.edit_ex.visibility = View.VISIBLE
+            itemView.copy_ex.visibility = View.VISIBLE
+            itemView.delete_ex.visibility = View.VISIBLE
+            itemView.arrow.animate().rotation(180f)
+        } else {
+            itemView.divider.visibility = View.GONE
+            itemView.edit_ex.visibility = View.GONE
+            itemView.copy_ex.visibility = View.GONE
+            itemView.delete_ex.visibility = View.GONE
+            itemView.arrow.animate().rotation(0f)
+        }
     }
 }
